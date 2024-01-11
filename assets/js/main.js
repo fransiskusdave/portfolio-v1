b4b12179d9797aba694bae08894c48e2d28ab984
Fransiskus Dave <davefransiskus@gmail.com>
2024-01-11 19:17:21 +0700

    adding light/dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,3 +87,34 @@ window.addEventListener("scroll", () => {
       : sectionsClass.classList.remove("active-link");
   });
 });
+
+// Dark Mode
+const themeButton = document.getElementById("theme-button"),
+  darkTheme = "dark-theme",
+  iconTheme = "ri-sun-fill";
+
+const selectedTheme = localStorage.getItem("selected-theme"),
+  selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-fill" : "ri-sun-fill";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+
+  themeButton.classList[selectedIcon === "ri-moon-fill" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
