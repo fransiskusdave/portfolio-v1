d85778d395909d25531125822f0a4344964f1040
Fransiskus Dave <davefransiskus@gmail.com>
2024-01-13 23:25:23 +0700

    improvement to scroll reveal animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,7 +129,7 @@ const sr = ScrollReveal({
 
 sr.reveal(".home__profil, .about__image, .contact__mail", { origin: "right" });
 sr.reveal(
-  ".home__name, .home__info, .about__container .section__title-1, .about__info, .contact__data",
+  ".home__name, .home__info, .about__container .section__title-1, .about__info, .contact__social, .contact__data",
   { origin: "left" }
 );
 sr.reveal(".services__card, .projects__card", { interval: 100 });
